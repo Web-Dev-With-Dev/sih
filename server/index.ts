a089da7b8a4e9474f51767071b6e71d3fcfcd778
Web-Dev-With-Dev <gondaliyadev007@gmail.com>
2025-09-02 03:11:47 +0530

    fourth

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { addDefaultMembers } from "./init-default-members";
 
 const app = express();
 app.use(express.json());
@@ -69,3 +70,5 @@ app.use((req, res, next) => {
     log(`serving on port ${port}`);
   });
 })();
+
+addDefaultMembers();
